25126d870562b8ecf595b6dcdd2366929ca30d2b
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-22 16:28:29 +0200

    add the show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,12 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(params[:article].permit(:title, :text))
+		@article.save
+		redirect_to @article
+	end
+
+	def show
+		@article = Article.find(params[:id])
 	end
 end
