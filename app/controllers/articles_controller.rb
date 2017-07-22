44a79dfd6560716048abc93a96ce7ad775af62b0
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-22 16:30:15 +0200

    refactor article_params strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(params[:article].permit(:title, :text))
+		@article = Article.new(article_params)
 		@article.save
 		redirect_to @article
 	end
@@ -11,4 +11,9 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
 end
