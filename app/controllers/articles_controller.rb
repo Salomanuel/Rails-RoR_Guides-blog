f12bf026a2780d7bc5221c42559a2be64f2a8a11
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-24 16:13:31 +0200

    implement #destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,19 @@
 class ArticlesController < ApplicationController
+	
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def new
 		@article = Article.new
 	end
 
 	def edit
 		@article = Article.find(params[:id])
-	end
-
-	def update
-		@article = Article.find(params[:id])
-		if @article.update(article_params)
-			redirect_to @article
-		else
-			render 'edit'
-		end
 	end
 
 	def create
@@ -25,12 +25,19 @@ class ArticlesController < ApplicationController
 		end
 	end
 
-	def show
+	def update
 		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
 	end
 
-	def index
-		@articles = Article.all
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
 	end
 
 	private
